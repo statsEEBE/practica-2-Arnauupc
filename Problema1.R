12e987811e494ba276aa3cf8a083be0fe4a7fc2b
Arnauupc <arnau.casanovas@estudiantat.upc.edu>
2024-10-03 11:32:27 +0200

    Update Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
 #Codigo para problema 1
 
+sqrt(0.1923)
+
+#variacio de market value en una unitat de canvio de assets serà el pendent que es el que demanen
+# m= 0.06576
+# valor predic per 6471
+
+m <- 0.0658
+b <- 11.798
+x <- 647.1
+
+m*x +b
+#Codigo para problema 1
+
